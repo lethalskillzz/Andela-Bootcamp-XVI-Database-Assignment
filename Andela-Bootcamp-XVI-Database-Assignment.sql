5f2eab6d4e906fc2aa0afcb07713458c9a5e4122
Ibrahim Amin <lethalskillzz@gmail.com>
2016-05-19 17:55:33 +0100

    add Andela-Bootcamp-XVI-Database-Assignment.sql

---
+++ Andela-Bootcamp-XVI-Database-Assignment.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `users` WHERE `username` = 'Ibrahim' <br>
+INSERT INTO `users` ( `username`, `email` ) VALUES ( 'Ibrahim', 'lethal@mail.com' ) <br>
+SELECT * FROM `items` WHERE `id` = 32 <br>
+UPDATE `items` SET `name` = 'Food Item' WHERE `id` = 12 <br>
+DELETE FROM `transactions` WHERE `user_id` = 3 AND `item_id` = 344 <br>
